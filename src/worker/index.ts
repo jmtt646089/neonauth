86a85f97fe3f79009e53fcdd5df64d4ef6b12ead
jmtt646089 <jadeseala@gmail.com>
2026-02-05 20:22:17 +1300

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -3,7 +3,12 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 app.post('/api/auth/sign-in/social', async (c) => {
-  return c.json({ message: 'Success' })
-})
+  return c.json({ message: 'Success - sign in social' })
+});
+
+
+app.get('api/auth/get-session', async (c) => {
+  return c.json({ message: 'Success - get session' })
+});
 
 export default app;
