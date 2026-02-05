98b06443f6508fccd26dd141c1ba5b4cf359519a
jmtt646089 <jadeseala@gmail.com>
2026-02-05 20:49:22 +1300

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
+/*
 app.get("/api", (c) => {
   console.log("success - api");
   c.json({ name: "Cloudflare" });
 });
+*/
 
 app.post('/api/auth/sign-in/social', async (c) => {
   console.log("success sign in social");
